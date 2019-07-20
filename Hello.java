a4c015a5e9eb4bcb8f36991543866f565367fb3b
cy145 <1459288522@qq.com>
2019-07-20 09:54:07 +0800

    Test Merge

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
 ﻿public class Hello{
 	public static void main(String[] args){
 		System.out.println("不认真听课的孩子去后面站着！");
+		System.out.println("不认真听课的孩子去后面站着！");
+		System.out.println("不认真听课的孩子去后面站着！");
 		System.out.println("世界，你好！");
 		System.out.println("hello！");
 		System.out.println("hello720");
